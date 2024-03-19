969937ac6f0e15163d8ea61c09d30f93ca70885d
niajeanpierre <nia.jeanpierre@mykenzie.snhu.edu>
2024-03-19 13:08:50 -0400

    css edits to sidebar & sidebtn

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -2,19 +2,25 @@ import React from 'react'
 import './sidebar.css'
 import image from './profile.jpeg'
 import SidebarButton from './sidebarButton'
+import { MdFavorite } from "react-icons/md";
+import { FaPlayCircle } from "react-icons/fa";
+import { MdLibraryMusic } from "react-icons/md";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoMdTrendingUp } from "react-icons/io";
+import { IoShareSocialSharp } from "react-icons/io5";
 
 const Sidebar = () => {
   return (
     <div className='sidebar-container'>
       <img src={image} className='profile-img' alt='profile' />
       <div>
-        <SidebarButton />
-        <SidebarButton />
-        <SidebarButton />
-        <SidebarButton />
-        <SidebarButton />
+        <SidebarButton title="Feed" to="/feed" icon={<IoShareSocialSharp />} />
+        <SidebarButton title="Trending" to="/trending" icon={<IoMdTrendingUp />} />
+        <SidebarButton title="Player" to="/player" icon={<FaPlayCircle />} />
+        <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />} />
+        <SidebarButton title="Library" to="/" icon={<MdLibraryMusic />} />
       </div>
-      <SidebarButton />
+      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
     </div>
   )
 }
